ec466f68ddb9d66e303027b57b28ea4e31e69d2c
Girinda <girindade@gmail.com>
2025-06-26 00:33:58 +0200

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,12 @@
+#advance concept- strings 
+
+message = 'hello'
+
+print(message[0])
+print(message[1])
+print(message[-1])
+
+
+print(message.strip()) #remove leading and trailing whitespace
+print(message.lower()) #convert all lower characters to lowercase
+print(message.split(",")) # split the string into a base the
