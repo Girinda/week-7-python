8a3b50ae8d89004429e3d08bca3028733a7cbe16
Girinda <girindade@gmail.com>
2025-06-26 18:36:43 +0200

    Create loops.python.py

---
+++ loops.python.py
@@ -0,0 +1,20 @@
+#loops
+
+fruits =["apple","banana","cherry"]
+
+for fruits in fruits:
+    print(fruit)
+
+numbers =[1,2,3,5,7]
+
+for number in number
+    print(number)
+
+
+
+
+count = 1
+
+while count < = 5:
+      print(count)
+      count + = 1 # increments the count by 1
