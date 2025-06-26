f1e1544789531f1422b7852654dd0e7ff5a7f09a
Girinda <girindade@gmail.com>
2025-06-26 18:07:30 +0200

    Create operators.pythong.py

---
+++ operators.pythong.py
@@ -0,0 +1,23 @@
+# addition (+)
+# subtraction (-)
+# multiplication (×)
+# division (/)
+# module (%)
+# exponent (**)
+
+× = 10
+y = 2
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+
+ x + = 2
+print (x)
+
+x - = 2
+print(x)
+
